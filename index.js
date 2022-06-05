ef5cafb25dee03df5cfe3bf115f4cc70c9b74933
kharis.blk <kharis.blk@gmail.com>
2022-06-05 21:24:26 +0700

    Edit index.js 01

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (daftarNama, fnUrutan) => {
+  const urutan = fnUrutan(daftarNama);
+  const hasil = [];
+  for (let index = 0; index < urutan.length; index++) {
+    // const nama = urutan[index];
+    hasil.push(index + 1 + ". " + daftarNama[index] );
+  }
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
-
+const sortAscending = (daftarNama) => {
+  daftarNama.sort();
+  return daftarNama;
+};
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (daftarNama) => {
+  daftarNama.sort().reverse();
+  return daftarNama;
+};
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
